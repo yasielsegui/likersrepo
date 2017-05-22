43951706983adf713aa11fa7454d0f2753507d5c
Yasiel Segui <yasielsegui@live.com>
2017-05-22 00:29:07 -0400

    adding  facebook service and development key hash

---
+++ app/src/main/java/co/likersapp/likers/LoginActivity.java
@@ -0,0 +1,9 @@
+package co.likersapp.likers;
+
+import android.app.Activity;
+
+/**
+ * Created by yasiel on 5/22/2017.
+ */
+public class LoginActivity extends Activity {
+}
---
+++ app/src/main/java/co/likersapp/likers/business/FacebookServices.java
@@ -0,0 +1,7 @@
+package co.likersapp.likers.business;
+
+/**
+ * Created by yasiel on 5/22/2017.
+ */
+public class FacebookServices {
+}
